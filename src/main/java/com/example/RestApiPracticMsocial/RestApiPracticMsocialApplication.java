1e075ccddc317db83c87dce20469487b27bd2e36
Maksim Moiseev <moiseev.maksim32@gmail.com>
2024-05-23 15:04:39 +0300

    Initial commit

---
+++ src/main/java/com/example/RestApiPracticMsocial/RestApiPracticMsocialApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestApiPracticMsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPracticMsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPracticMsocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestApiPracticMsocial/RestApiPracticMsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestApiPracticMsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPracticMsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
